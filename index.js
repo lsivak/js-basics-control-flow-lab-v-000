b50688e6fcbe77a582c525d7a5578009119f9530
Lara Sanders <larasanders@gmail.com>
2018-05-16 00:03:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code in this file!
+function scuberGreetingForFeet(someNumber) {
+  let result
+  if (someNumber <= 400) {
+    result = 'This one is on me!'
+  } else if (someNumber < 2500 ) {
+    result = 'I will gladly take your thirty bucks.'
+  } else if (someNumber > 2500)
+    result = 'No can do.'
+  return result;
+}
