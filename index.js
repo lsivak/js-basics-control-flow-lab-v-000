6d949c15739c39ed25ad81513fea0d6397a3c513
Lara Sanders <larasanders@gmail.com>
2018-05-16 15:58:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ function scuberGreetingForFeet(someNumber) {
   return result;
 }
 
-// let ternaryCheckCity(NYC) ? 'Ok, sounds good.' : 'No go.'
+function ternaryCheckCity(NYC) ? 'Ok, sounds good.' : 'No go.'
