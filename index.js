27f253e24ca6f2356ab9e188e569b2048a943399
Lara Sanders <larasanders@gmail.com>
2018-05-16 16:58:15 +0000

    Done.

--- index.js
+++ index.js
@@ -8,3 +8,27 @@ function scuberGreetingForFeet(someNumber) {
     result = 'No can do.'
   return result;
 }
+
+
+function ternaryCheckCity(city) {
+  result = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return result;
+}
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case 'generous':
+    tip > 10
+    return 'Thank you so much.';
+    break
+    case 'not as generous':
+    tip <= 10
+    return 'Thank you.';
+    break
+    case 'thanks for everything':
+    tip <= 5
+    default:
+     return 'Bye.'
+    break
+  }
+}
